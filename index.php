1cfe69b90307a530bc4a860bb778e739cf874da1
revell29 <diraapsya0@gmail.com>
2019-07-16 17:21:16 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+    <link rel="manifest" href="/manifest.json" />
+
+    <title>Apsyadira</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="assets/js/modernizr-2.6.2.min.js"></script>
+
+
+
+</head>
+
+<body>
+    <div id="colorlib-page">
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="colorlib-navbar-brand">
+                            <a class="colorlib-logo" href="index.html"><span>Apsya</span><span>Dira</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- <Header /> -->
+        <div id="colorlib-about">
+            <div class="container">
+                <div class="row text-center">
+                    <h2 class="bold">Hello</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-5 animate-box">
+                        <div class="owl-carousel3" ref="owl1">
+                            <div class="item">
+                                <img class="img-responsive about-img" src="/assets/images/dira1.jpg"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive about-img" src="/assets/images/dira2.jpg"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-md-push-1 animate-box">
+                        <div class="about-desc">
+                            <div class="owl-carousel3" ref="owl3">
+                                <div class="item">
+                                    <h2><span>I'm</span><span>Apsyadira</span></h2>
+                                </div>
+                                <div class="item">
+                                    <h2><span>I'm</span><span>A Software Engineer</span></h2>
+                                </div>
+                            </div>
+                            <div class="desc">
+                                <div class="rotate">
+                                    <h2 class="heading">Describe</h2>
+                                </div>
+                                <p>I am a fullstack developer. I really like the challenges and new things I can
+                                    learn.
+                                    I really like the world of technology and the world of programming. I am also
+                                    active in several programming groups in my country. I work with web frameworks
+                                    like Laravel, ReactJS, Django and React Native for making mobile applications. I
+                                    have 2 years experience in building a product from scratch, developing and
+                                    integrating APIs.</p>
+                                <p class="colorlib-social-icons">
+                                    <a href="https://www.youtube.com/channel/UCCNR2s-skIhAZKCfy3EXXdA">
+                                        <i class="icon-youtube"></i>
+                                    </a>
+                                    <a href="https://twitter.com/apsyadiraa"><i class="icon-twitter"></i></a>
+                                    <a href="https://www.instagram.com/apsyadiraa/"><i class="icon-instagram"></i></a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Service -->
+        <div id="colorlib-services">
+            <div class="container">
+                <div class="row text-center">
+                    <h2 class="bold">Service</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="services-flex">
+                            <div class="one-third">
+                                <div class="row">
+                                    <div class="col-md-12 col-md-offset-0 animate-box intro-heading">
+                                        <span>My Services</span>
+                                        <h2>Here Are Some of My Skills</h2>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="rotate">
+                                            <h2 class="heading">Skills</h2>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="services animate-box">
+                                            <h3>1 - Backend & Server</h3>
+                                            <ul>
+                                                <li>PHP 7</li>
+                                                <li>Node.js</li>
+                                                <li>SQL, NoSQL, MongoDB</li>
+                                                <li>RESTfull API</li>
+                                                <li>Laravel, Codeigniter, Django</li>
+                                                <li>Version Control (GIT)</li>
+                                                <li>Ubuntu, Centos, FreeBSD</li>
+                                                <li>Hosting, Cloud, Domain</li>
+                                            </ul>
+                                        </div>
+                                        <div class="services animate-box">
+                                            <h3>2 - Frontend</h3>
+                                            <ul>
+                                                <li>HTML</li>
+                                                <li>CSS</li>
+                                                <li>JS</li>
+                                                <li>React JS & Vue JS</li>
+                                                <li>React Native</li>
+                                                <li>Bootstrap</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+
+                                        <div class="services animate-box">
+                                            <h3>3 - Photography</h3>
+                                            <ul>
+                                                <li>Adobe Photoshop</li>
+                                                <li>Sony Vegas</li>
+                                            </ul>
+                                        </div>
+                                        <div class="services animate-box">
+                                            <h3>4 - Soft skills</h3>
+                                            <ul>
+                                                <li>Badminton</li>
+                                                <li>Futsal</li>
+                                                <li>Guitar & Drummer</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="one-forth services-img"
+                                style='background-image: url(assets/images/image-laptop.jpg);'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Service -->
+
+        <div id="colorlib-work">
+            <div class="container">
+                <div class="row text-center">
+                    <h2 class="bold">Portofolio</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
+                        <span>Portfolio</span>
+                        <h2>My Portofolio</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="rotate">
+                            <h2 class="heading">Portfolio</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 animate-box">
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p1.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p2.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 animate-box">
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p3.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p4.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 animate-box">
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p5.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p6.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 animate-box">
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p7.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p8.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 text-center animate-box">
+                        <div class="porto-entry">
+                            <img class="porto-img" src="/assets/images/p9.png"
+                                    alt="html5 bootstrap template by colorlib.com" />
+                                <div class="middle">
+                                    <div class="text">John Doe</div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Work -->
+
+
+        <!-- Quote -->
+        <div id="colorlib-testimony">
+            <div class="container">
+                <div class="row text-center">
+                    <h2 class="bold">AweSOme</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
+
+                        <h2>Quotes</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="rotate">
+                            <h2 class="heading">Says</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row animate-box">
+                    <div class="owl-carousel">
+                        <div class="item">
+                            <div class="col-md-12 text-center">
+                                <div class="testimony">
+                                    <blockquote>
+                                        <p>"Leave what you must, abandon who you must, sacrifice whatever you must -
+                                            just to preserve yout faith and avoid fitna in your din"</p>
+                                        <span>" &mdash; Surah Al-Kahf"</span>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-md-12 text-center">
+                                <div class="testimony">
+                                    <blockquote>
+                                        <p>"He will give you [something] better than what was taken form you"</p>
+                                        <span>" &mdash; Qur'an 8:70"</span>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-md-12 text-center">
+                                <div class="testimony">
+                                    <blockquote>
+                                        <p>"Do not grieve, indeed Allah is with us"</p>
+                                        <span>" &mdash; Qur'an 9:40</span>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Quote  -->
+
+        <footer>
+            <div id="footer">
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <p>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                copyright © 2018 | Develop by Apsyadira
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- <Footer /> -->
+    </div>
+    <!-- jQuery -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="assets/js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <!-- Owl Carousel -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/magnific-popup-options.js"></script>
+    <!-- Main JS (Do not remove) -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
